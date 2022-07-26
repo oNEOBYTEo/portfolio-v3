2c5884ab7dbb833cdb4ea1e4bb84d2fd5d93b94c
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-07-25 20:18:46 -0500

    format fix

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -25,14 +25,14 @@ const Project = () => {
               className={`project__semicarousel carouse-movement-${imgSelected}`}
             >
               <img
-                src="/pokeapi.png"
+                src="/pokeapi.webp"
                 alt="pokeapi"
                 className={`project__items ${
                   imgSelected === 1 && 'carousel-image-active'
                 }`}
               />
               <img
-                src="/victory-jewel.png"
+                src="/victory-jewel.webp"
                 alt="victroy jewel"
                 className={`project__items ${
                   imgSelected === 2 && 'carousel-image-active'
@@ -69,14 +69,14 @@ const Project = () => {
               id="slide-1"
               onScroll={(e) => console.log('hola')}
             >
-              <img src="/pokeapi.png" alt="pokeapi" />
+              <img src="/pokeapi.webp" alt="pokeapi" />
             </div>
             <div
               className="project__slide"
               id="slide-2"
               onClick={() => console.log('hola')}
             >
-              <img src="/victory-jewel.png" alt="victroy jewel" />
+              <img src="/victory-jewel.webp" alt="victroy jewel" />
             </div>
             <div
               className="project__slide"
